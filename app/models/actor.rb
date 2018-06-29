5a6b4349fcd0f858b629bd5ca8d55f99ff387315
Kachel Wittig <kjwittig@gmail.com>
2018-06-29 02:05:51 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,7 +2,9 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
-  def self.full_name
+  def full_name
     self.first_name + " " + self.last_name
   end
+
+  def
 end
