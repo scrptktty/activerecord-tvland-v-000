5f4bb908322027a2816fb2079040b3ea3a412997
Kachel Wittig <kjwittig@gmail.com>
2018-06-29 01:55:51 +0000

    automatically backed up by learn

--- db/migrate/006_add_id_to_character.rb
+++ db/migrate/006_add_id_to_character.rb
@@ -1,4 +1,4 @@
-class AddidtoCharacter < ActiveRecord::Migration[4.2]
+class AddIdToCharacter < ActiveRecord::Migration[4.2]
   def change
     add_column :characters, :character_id, :integer
   end
