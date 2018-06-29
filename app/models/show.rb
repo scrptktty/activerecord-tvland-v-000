bc51719df94ea2090850745d09292c4272a4a087
Kachel Wittig <kjwittig@gmail.com>
2018-06-29 02:37:51 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,10 @@
+require "pry"
 class Show < ActiveRecord::Base
   has_many :characters
   belongs_to :network
 
   def build_network
-    pry
+    binding.pry
     self.network_id.call_letters
   end
 
