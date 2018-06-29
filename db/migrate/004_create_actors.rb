4a35d6d5db1fba9cd569b16dbe0b3756aecf3635
Kachel Wittig <kjwittig@gmail.com>
2018-06-29 01:28:38 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,7 +1,7 @@
 class CreateActors < ActiveRecord::Migration[4.2]
   def change
     create_table :actors do |t|
-      #primary key of :id is created for us!
+
       t.string :first_name
       t.string :last_name
     end
